345fda3b8f20a3c51bb5134c371decb97daff1fc
Vijay Ganesan <vijayganesan1@yahoo.ca>
2000-01-01 02:38:21 +0000

    Edited ultrasonic test file, next step is to figure out why GPIO isn't working

--- BBB/maxbotix.py
+++ BBB/maxbotix.py
@@ -1,16 +1,25 @@
 from Adafruit_I2C import Adafruit_I2C
+import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
 
 # Maxbotix Sensor Documentation: http://maxbotix.com/documents/I2CXL-MaxSonar-EZ_Datasheet.pdf
 
 i2c = Adafruit_I2C(0x70)  # Default Sensor address. Can be changed with other I2C commands
+GPIO.setup("P8_14",GPIO.IN)
 
 while True:
-    #i2c.write8(0, 0x51)  # Take Range Reading
     i2c.write8_noreg(0x51)
+    
+    if GPIO.input("P8_14"):
+        print("HIGH")
+    else:
+        print("LOW")
 
-    # Not sure what the correct sleep value should be.
-    # With the timing analyzer I was seeing that the status pin was high for around 15ms.
-    # Even with this 0.1 value it was still saying the device is inaccessible when it was reading really long distances.
-    sleep(0.1)
+    sleep(0.1) #We really only need to wait 50 us to check
+
+    if GPIO.input("P8_14"):
+        print("HIGH")
+    else:
+        print("LOW")
+
     print i2c.reverseByteOrder(i2c.readU16(0x0))  # Read latest range reading
