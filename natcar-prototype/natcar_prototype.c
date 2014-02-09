4e8f6083c06c503d56c3ee2c5c460296077d81a1
Scotty Waggoner <ozzieorca@gmail.com>
2014-02-09 01:43:12 -0800

    Added basic PWM outputs for steering servo. Currently D0 makes the servo turn left and D1 makes it turn right

--- natcar-prototype/natcar_prototype.c
+++ natcar-prototype/natcar_prototype.c
@@ -23,37 +23,31 @@ int main(void){
 	SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
 
 	// Enable the peripherals used by this program.
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);  //The Tiva Launchpad has two modules (0 and 1). Module 1 covers the LED pins
-	ulPeriod = SysCtlClockGet() / 200; //PWM frequency 200HZ
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+	ulPeriod = SysCtlClockGet() / 200 * 4; //PWM frequency 200HZ
 
-	//Configure PF1,PF2,PF3 Pins as PWM
-	GPIOPinConfigure(GPIO_PF1_M1PWM5);
-	GPIOPinConfigure(GPIO_PF2_M1PWM6);
-	GPIOPinConfigure(GPIO_PF3_M1PWM7);
-	GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+	//Configure PD0 and PD1 Pins as PWM
+	GPIOPinConfigure(GPIO_PD0_M1PWM0);
+	GPIOPinConfigure(GPIO_PD1_M1PWM1);
+	GPIOPinTypePWM(GPIO_PORTD_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
 	//Configure PWM Options
-	//PWM_GEN_2 Covers M1PWM4 and M1PWM5
-	//PWM_GEN_3 Covers M1PWM6 and M1PWM7 See page 207 4/11/13 DriverLib doc
-	PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
-	PWMGenConfigure(PWM1_BASE, PWM_GEN_3, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+	//PWM_GEN_0 Covers M1PWM0 and M1PWM1 See p.153 in the ROM User's Guide
+	PWMGenConfigure(PWM1_BASE, PWM_GEN_0, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
 
 	//Set the Period (expressed in clock ticks)
-	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, ulPeriod);
-	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_3, ulPeriod);
+	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_0, ulPeriod);
 
 	//Set PWM duty-50% (Period /2)
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5,ulPeriod/100);
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_6,ulPeriod/100);
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7,ulPeriod/100);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0,ulPeriod/17);
+	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_1,ulPeriod/13);
 
 	// Enable the PWM generator
-	PWMGenEnable(PWM1_BASE, PWM_GEN_2);
-	PWMGenEnable(PWM1_BASE, PWM_GEN_3);
+	PWMGenEnable(PWM1_BASE, PWM_GEN_0);
 
 	// Turn on the Output pins
-	PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT |PWM_OUT_6_BIT|PWM_OUT_7_BIT, true);
+	PWMOutputState(PWM1_BASE, PWM_OUT_0_BIT | PWM_OUT_1_BIT, true);
 
 	//Do nothing
 	while(1)
