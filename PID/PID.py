77647a772b7f1a960ce27bf0a52e72f6cb95e695
Scotty Waggoner <ozzieorca@gmail.com>
2014-04-10 02:20:40 -0700

    Fix class errors in PID

--- PID/PID.py
+++ PID/PID.py
@@ -3,7 +3,9 @@
 
 @author: Evan Racah
 '''
+
 import time
+
 
 class PIDControl(object):
     def __init__(self, setpoint, coefficients, maxSpeed):  #maxSpeed is in rads/sec i think?
@@ -19,26 +21,26 @@ class PIDControl(object):
     def update(self, measurement):
         self.now = time.time()
         self.timeInterval = self.now - self.lastTime
-        calcErrorTerms(measurement)
-        self.input = calcInput()
+        self.calcErrorTerms(measurement)
+        self.input = self.calcInput()
         self.lastTime = self.now
         self.previousError = self.error
-        return convertTo8Bit(self.input)
+        return self.convertTo8Bit(self.input)
 
-    def convertTo8Bit(speedDiff):
-        return (speedDiff / maxSpeed) * 127
+    def convertTo8Bit(self, speedDiff):
+        return (speedDiff / self.maxDiff) * 127
 
 
-    def calcErrorTerms(measurement):
-        self.error = max(min(self.setpoint - measurement, self.max_error),self.min_error)
+    def calcErrorTerms(self, measurement):
+        self.error = max(min(self.setpoint - measurement, self.max_error), self.min_error)
         self.errorTotal += self.error * self.timeInterval
         self.dError = (self.error - self.previousError) / self.timeInterval
 
-    def calcInput():
+    def calcInput(self):
         self.P = self.kP * self.error
         self.I = self.kI * self.errorTotal * self.timeInterval
         self.D = self.kD * (self.dError / self.timeInterval)
         return self.P + self.I + self.D
 
-    def set_set_point(self,setpoint):
+    def set_set_point(self, setpoint):
         self.setpoint = setpoint
