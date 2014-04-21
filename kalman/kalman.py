980d440fc7a61de4c0768e1141ca3484a03cdd7e
Evan Racah <ejracah@gmail.com>
2014-04-21 15:03:30 -0700

    Got a kalman implementation online and put it in

--- kalman/kalman.py
+++ kalman/kalman.py
@@ -1,13 +1,45 @@
 #Kalman Filter 
-#Evan, Oliver 4/16/14
-import cv2
+#This class was written entirely by Greg Czerniak with some modifications
+#and can be found here: http://greg.czerniak.info/guides/kalman1/
+#import numpy
 
-class Kalman(object):
-	def __init__(self,params,matrices): #params is a list of the number of elements in the F,B, and H matrix
-		self.KalmanFilt = cv2.KalmanFilter(params)
-		self.F, self.B, self.G,self.H,self.G,self.Z,self.P0 = matrices
-		#set matrices inside kalman filter
+#we need numpy here...
+import numpy as np
+import time
 
+# Implements a linear Kalman filter.
+class KalmanFilterLinear(object):
+  def __init__(self,_A, _B, _H, _x, _P, _Q, _R):
+    self.A = _A                      # State transition matrix.
+    self.B = _B                      # Control matrix.
+    self.H = _H                      # Observation matrix.
+    self.current_state_estimate = _x # Initial state estimate.
+    self.current_prob_estimate = _P  # Initial covariance estimate.
+    self.Q = _Q                      # Estimated error in process.
+    self.R = _R                      # Estimated error in measurements.
+    self.startTime = time.time()
+  def GetCurrentState(self):
+    return self.current_state_estimate
+  def Step(self,control_vector,measurement_vector):
+    #---------------------------Prediction step-----------------------------
+    predicted_state_estimate = self.A * self.current_state_estimate + self.B * control_vector
+    predicted_prob_estimate = (self.A * self.current_prob_estimate) * numpy.transpose(self.A) + self.Q
 
+    #--------------------------Observation step-----------------------------
+    innovation = measurement_vector - self.H*predicted_state_estimate
+    innovation_covariance = self.H*predicted_prob_estimate*numpy.transpose(self.H) + self.R
+
+    #-----------------------------Update step-------------------------------
+    kalman_gain = predicted_prob_estimate * numpy.transpose(self.H) * numpy.linalg.inv(innovation_covariance)
+    self.current_state_estimate = predicted_state_estimate + kalman_gain * innovation
+
+    # We need the size of the matrix so we can make an identity matrix.
+    size = self.current_prob_estimate.shape[0]
+    # eye(n) = nxn identity matrix.
+    self.current_prob_estimate = (numpy.eye(size)-kalman_gain*self.H)*predicted_prob_estimate
+		
+if __name__ == "__main__":
+	dt = 0.0001 #we need to calculate this
+	A = np.matrix([[1, 0, dt, 0 ],[0, 1, 0, dt],[])
 
 
