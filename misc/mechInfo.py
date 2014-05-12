b77f0340825849ad4083d0f12d10a7823d8b5433
Scotty Waggoner <ozzieorca@gmail.com>
2014-05-12 01:34:32 -0700

    Changed desired speed to 0.75 to match motor_test.py. We should discuss the speed we want.

--- misc/mechInfo.py
+++ misc/mechInfo.py
@@ -5,7 +5,7 @@ from math import pi
 wheelDiameter = 31.75  # cm, 12.5 inches
 wheelCircumference = wheelDiameter * pi
 robotWidth = 25.4  # cm, 10 inches
-desiredSpeed = 0.5  # rps, desiredSpeed in rps
+desiredSpeed = 0.75  # rps, desiredSpeed in rps
 distBetweenUS = 20  # cm
 maxPPS = 4600  # encoder reading at max speed (pulses per second)
 distForNoFurrow = 50  # cm, distances greater than this are assumed to be at the end of the furrow
