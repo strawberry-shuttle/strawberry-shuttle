4b5ab61e082aa7e0afc91f5a478834a7f51580e7
Evan Racah <ejracah@gmail.com>
2014-05-17 15:53:56 -0700

    added time response data and matlab script for plotting it

---
+++ matlab_scripts/readAndPlot.m
@@ -0,0 +1,34 @@
+function [t, data] = readAndPlot(varargin)
+
+if length(varargin)> 3
+    error('Only two input args allowed')
+end
+
+optargs = {'./TestResults/','timedata.txt','RPS_TimeResponse1.txt'};
+optargs(1:length(varargin)) = varargin;
+[directory, tfilename, filename] = optargs{:};
+    
+fileID = fopen([directory tfilename],'r');
+
+
+
+formatSpec = '%f';
+
+t = fscanf(fileID,formatSpec)
+fclose(fileID);
+
+count = 1;
+data = zeros(length(t),4);
+
+while (count <5)
+    filename(end - length('.txt')) = num2str(count);
+    fileID = fopen([directory filename],'r');
+    data(:,count) = fscanf(fileID,formatSpec)
+    fclose(fileID);
+    subplot(2,2,count),plot(t,data(:,count));
+    title(['Motor ' num2str(count)])
+    ylabel('Speed (Rotations Per Second)')
+    xlabel('Time (s)')
+    count = count + 1;
+    
+end
