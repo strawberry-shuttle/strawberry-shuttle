23f10a91172b0756863fcc4b0420c3955e60b58e
Scotty Waggoner <ozzieorca@gmail.com>
2014-06-06 14:22:59 -0700

    Added command line arguments to motor_test to set custom speeds. Reduced distance to ~10 yards

--- motor_test.py
+++ motor_test.py
@@ -4,8 +4,17 @@ from drivers.motors import Motors
 from time import sleep, time
 from misc import mechInfo
 import Adafruit_BBIO.GPIO as GPIO
+import argparse
 
-distance = 1828.0  # distance to travel in cm, ~20 yards
+parser = argparse.ArgumentParser(description='Run motors with specified speed')
+parser.add_argument('leftSpeed', type=float, nargs='?', default="0.5", help='Speed for the left wheels in revolutions per second')
+parser.add_argument('rightSpeed', type=float, nargs='?', default="0.5", help='Speed for the right wheels in revolutions per second')
+
+args = parser.parse_args()
+print args.leftSpeed
+print args.rightSpeed
+
+distance = 914.0  # distance to travel in cm, ~10 yards
 rps = 0.75  # Velocity
 
 rotations = distance/mechInfo.wheelCircumference
@@ -19,7 +28,7 @@ while 1:
     while GPIO.input("P8_9"):
         pass
 
-    m.moveForward(0,0.75)
+    m.move(args.leftSpeed, args.rightSpeed)
     m.printEncoderSpeeds()
     
     sleep(2)
@@ -33,4 +42,3 @@ while 1:
 
     m.moveForward(0, 0)
     sleep(3)
-
